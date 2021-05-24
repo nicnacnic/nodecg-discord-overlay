1deef2317b0decc2516e0b90fde0121682086412
nicnacnic <nicnacnicyt@gmail.com>
2021-05-24 00:57:23 -0400

    Add files via upload

--- extension/commands.js
+++ extension/commands.js
@@ -1,11 +1,11 @@
 module.exports = (client, alias, roleID, callback) => {
 	client.on('message', (message) => {
-		const content = message.content.split(' ');
+		const { content } = message;
+		const command = `<@!${client.user.id}> ${alias}`;
 
-		if (content.length > 1 && content[0] === `<@!${client.user.id}>` && content[1] === alias) {
-			if (message.member.hasPermission('ADMINISTRATOR') || message.member.hasPermission('MANAGE_CHANNELS') || message.member.roles.cache.has(roleID)) {
-					callback(message)
-			}
+		if (content === command) {
+			if (message.member.hasPermission('ADMINISTRATOR') || message.member.hasPermission('MANAGE_CHANNELS') || message.member.roles.cache.has(roleID))
+				callback(message)
 			else
 				message.reply(`you do not have permission to execute this command!`)
 		}
--- extension/index.js
+++ extension/index.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
-const Speaker = require('speaker');
-const AudioMixer = require('audio-mixer')
 const Discord = require('discord.js');
 const command = require('./commands');
 
-let silence, connection, guild;
+let silence, connection;
 
 module.exports = function (nodecg) {
 
-	let currentMembers = [];
 	const memberList = nodecg.Replicant('memberList', { persistent: false });
 	const addMember = nodecg.Replicant('addMember', { persistent: false });
 	const removeMember = nodecg.Replicant('removeMember', { persistent: false });
@@ -36,36 +33,31 @@ module.exports = function (nodecg) {
 		});
 
 		command(client, 'connect', roleID, (message) => {
-			if (connection === undefined && message.member.voice.channel !== undefined && message.member.voice.channel !== null) {
-				guild = message.member.guild.id;
+			if (connection === undefined && message.member.voice.channel !== undefined && message.member.voice.channel !== null)
 				record(message.member.voice.channel.id);
-			}
+			else
+				message.reply(`you're not in a voice channel!`)
 		})
 
 		command(client, 'disconnect', roleID, (message) => {
-			if (connection !== undefined) {
+			if (connection !== undefined)
 				stopRecording(connection.channel.name);
-			}
+			else
+				message.reply(`I'm not in a voice channel!`)
 		});
 
 		client.on('guildMemberSpeaking', (member, memberSpeaking) => {
-			for (let i = 0; i < currentMembers.length; i++) {
-				if (currentMembers[i].id === member.id) {
-					let speakState;
-					if (memberSpeaking.bitfield == 1)
-						speakState = true;
-					else
-						speakState = false;
-					speaking.value = null;
-					speaking.value = { id: member.id, speaking: speakState }
-				}
-			}
+			if (memberSpeaking.bitfield == 1)
+				speakState = true;
+			else
+				speakState = false;
+			speaking.value = null;
+			speaking.value = { id: member.id, speaking: speakState }
 		});
 
 		client.on('voiceStateUpdate', (oldMember, newMember) => {
 			if (connection !== undefined && newMember.id !== client.user.id) {
 				if (oldMember.channelID !== connection.channel.id && newMember.channelID === connection.channel.id) {
-					currentMembers.push({ id: newMember.id, audio: '', mixer: '' })
 					let muteState;
 					if (newMember.selfMute || newMember.selfDeaf || newMember.serverMute || newMember.serverDeaf)
 						muteState = true;
@@ -76,12 +68,6 @@ module.exports = function (nodecg) {
 					memberList.value.push({ id: newMember.id, name: newMember.member.user.username, avatar: newMember.member.user.displayAvatarURL(), muted: muteState });
 				}
 				else if (oldMember.channelID === connection.channel.id && newMember.channelID !== connection.channel.id) {
-					for (let i = 0; i < currentMembers.length; i++) {
-						if (currentMembers[i].id === newMember.id) {
-							currentMembers.splice(i, 1)
-							break;
-						}
-					}
 					for (let i = 0; i < memberList.value.length; i++) {
 						if (memberList.value[i].id === newMember.id) {
 							memberList.value.splice(i, 1)
@@ -92,8 +78,8 @@ module.exports = function (nodecg) {
 					removeMember.value = newMember.id;
 				}
 				else if (newMember.serverMute !== oldMember.serverMute || newMember.serverDeaf !== oldMember.serverDeaf || newMember.selfMute !== oldMember.selfMute || newMember.selfDeaf !== oldMember.selfDeaf) {
-					for (let i = 0; i < currentMembers.length; i++) {
-						if (currentMembers[i].id === newMember.id) {
+					for (let i = 0; i < memberList.value.length; i++) {
+						if (memberList.value[i].id === newMember.id) {
 							let muteState;
 							if (newMember.serverMute || newMember.serverDeaf || newMember.selfMute || newMember.selfDeaf)
 								muteState = true;
@@ -101,65 +87,50 @@ module.exports = function (nodecg) {
 								muteState = false;
 							changeMute.value = null;
 							changeMute.value = { id: newMember.id, muted: muteState }
-
-							for (let i = 0; i < memberList.value.length; i++) {
-								if (memberList.value[i].id === newMember.id) {
-									memberList.value[i].muted = muteState;
-									break;
-								}
-							}
-							break;
+							memberList.value[i].muted = muteState;
 						}
 					}
 				}
 			}
-			else if (oldMember.id === client.user.id && oldMember.channelID !== null) {
-				if (newMember.channelID === null)
-					stopRecording(oldMember.channel.name);
-				else if (oldMember.channelID !== newMember.channelID && connection !== undefined) {
-					let channelID = newMember.channelID;
-					stopRecording(oldMember.channel.name);
-					setTimeout(function () { record(channelID); }, 500);
-				}
+			else if (connection !== undefined && oldMember.channelID !== null) {
+				stopRecording(oldMember.channel.name);
+				if (newMember.channelID !== null && newMember.channelID !== connection.channel.id)
+					setTimeout(function () { record(newMember.channelID); }, 500);
 			}
 		})
 	});
 	async function record(channelID) {
 		connection = await client.channels.cache.get(channelID).join();
 
-		if (client.channels.cache.get(channelID).members.size > 1) {
-			client.channels.cache.get(channelID).members.forEach((member) => {
-				if (member.user.id !== client.user.id) {
-					currentMembers.push({ id: member.user.id, audio: '', mixer: '' });
-					let muteState;
-					if (member.voice.selfMute || member.voice.selfDeaf || member.voice.serverMute || member.voice.serverDeaf)
-						muteState = true;
-					else
-						muteState = false;
-					addMember.value = null;
-					addMember.value = { id: member.user.id, name: member.user.username, avatar: member.user.displayAvatarURL(), muted: muteState };
-					memberList.value.push({ id: member.user.id, name: member.user.username, avatar: member.user.displayAvatarURL(), muted: muteState });
-				}
-			})
-		}
+		client.channels.cache.get(channelID).members.forEach((member) => {
+			if (member.user.id !== client.user.id) {
+				let muteState;
+				if (member.voice.selfMute || member.voice.selfDeaf || member.voice.serverMute || member.voice.serverDeaf)
+					muteState = true;
+				else
+					muteState = false;
+				addMember.value = null;
+				addMember.value = { id: member.user.id, name: member.user.username, avatar: member.user.displayAvatarURL(), muted: muteState };
+				memberList.value.push({ id: member.user.id, name: member.user.username, avatar: member.user.displayAvatarURL(), muted: muteState });
+			}
+		})
 
 		silence = setInterval(function () {
 			connection.play(fs.createReadStream('./bundles/nodecg-dacbot/sounds/silence.wav'));
 		}, 270000)
 		nodecg.log.info('Capture started for channel ' + connection.channel.name + ' on ' + Date());
-		return;
 	}
+
 	function stopRecording(channelName) {
 		if (connection !== undefined) {
 			nodecg.log.info('Capture stopped for channel ' + channelName + ' on ' + Date())
 			connection.channel.leave();
 		}
-		for (let i = 0; i < currentMembers.length; i++) {
+		for (let i = 0; i < memberList.value.length; i++) {
 			removeMember.value = null;
-			removeMember.value = currentMembers[i].id;
+			removeMember.value = memberList.value[i].id;
 		}
 		memberList.value = [];
-		currentMembers = [];
 		connection = undefined;
 		clearInterval(silence);
 	}
